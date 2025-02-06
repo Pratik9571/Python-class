1f8309eb2dce9fb3fd3879cd30a213f732fa6151
Pratik Prajapati <prajapatipratik798@gmail.com>
2025-02-06 21:25:11 +0545

    Initial commit

---
+++ WEEK1_Assignment.py
@@ -0,0 +1,37 @@
+
+#? Question : 1
+# a = float(input("Enter any decimal number: "))
+# b = print("The number converted into integer would be:", int(a))
+# c = print("The number converted into string would be:", str(a))
+
+#? Question : 2
+# fullName = input("Enter your Full name: ");
+# nameSeparation = fullName.find(" ")
+
+# if nameSeparation!=-1 :
+#     firstName = fullName[0]
+#     lastName = fullName[nameSeparation+1][0]
+    
+#     print("Your initials are: ", (firstName+lastName).upper())
+
+#? Question : 3
+# a = str(input("Enter any word: "))
+# reverse_string = a[::-1]
+# print("The reversed word is: ",reverse_string)
+
+#? Question : 4
+# word = input("Enter a word: ")
+# starting_index = int(input("Enter starting index: "))
+# indexChanged = word[starting_index:]
+# print(f"The substring from index {starting_index} is: ",indexChanged)
+
+#? Question : 5
+# email= input("Enter an email address: ")
+# domainName = email.find("@")
+# domain = email[domainName +1: ]
+# print("Domain: ", domain)
+
+#? Question : 6
+# a = input("Enter any word: ")
+# swapWord= a[-1]+a[1:-1]+ a[0]
+# print("The Swapped word is: ", swapWord)
---
+++ day01.py
@@ -0,0 +1,16 @@
+# print ("Hello World")
+
+# c = 6
+# d=11
+# print(c+d)
+
+
+# a = str(6)
+# b= str(10)
+# print (a + b)
+
+a= input("Enter the first name:")
+b = input("Enter the last name:  ")
+
+c = (a + b)
+print("The name of the person is ", c )
